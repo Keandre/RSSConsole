300f9abcbd4ce7a59147a1cfddde3f656f2684fa
fanfreak2247 <keandrewinston@gmail.com>
2020-11-08 23:46:17 -0500

    Add files via upload

--- feed.py
+++ feed.py
@@ -1,18 +1,33 @@
 import feedparser
 import delorean
-from dateutil import parser 
-from datetime import datetime, timezone 
+from dateutil import parser
+from datetime import datetime, timezone
+
+
+class FeedItem():
+    def __init__(self, author, rss_item):
+        self.author = author
+        self.rss_item = rss_item
+        self.title = rss_item['title']
+        self.link = rss_item['link']
+
+class InvalidRSSError(Exception):
+    pass 
+
 
 class Feed():
     def __init__(self, link):
-        """Creates feed object with all of the latest things."""
-        self.updated_feed =[]
-        feed = feedparser.parse(link)
-        for item in feed['items']:
-            date_of = parser.parse(item['published'])
-            time_difference = datetime.now(timezone.utc) - date_of 
-            time_difference_hours = (time_difference.days * 24) + time_difference.seconds//3600
-            if time_difference_hours < 10:
-                self.updated_feed.append(item)
-        self.author = feed['feed']['title']
-
+        try:
+            """Creates feed object with all of the latest things."""
+            self.updated_feed = []
+            feed = feedparser.parse(link)
+            self.author = feed['feed']['title']
+            for item in feed['items']:
+                date_of = parser.parse(item['published'])
+                time_difference = datetime.now(timezone.utc) - date_of
+                time_difference_hours = (
+                    time_difference.days * 24) + time_difference.seconds//3600
+                if time_difference_hours < 48:
+                    self.updated_feed.append(FeedItem(self.author, item))
+        except:
+            raise InvalidRSSError()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from prettytable import PrettyTable
-from feed import Feed
+from feed import Feed, FeedItem, InvalidRSSError
 import webbrowser
 import os
 
 table = PrettyTable()
 table.field_names = ["#", "Author", "Title"]
 
-file_obj = open('links.txt')
+file_obj = open('links.txt', 'r')
 
 rsslinks = file_obj.readlines()
 
@@ -27,11 +27,11 @@ for i in range(len(rsslinks)):
     feed_objects.append(Feed(link))
 print("")
 
-for link_list in feed_objects:
-    entire_feed += link_list.updated_feed
+for feedobject in feed_objects:
+    entire_feed += feedobject.updated_feed
 
 for i in range(len(entire_feed)):
-    table.add_row([i+1, entire_feed[i].author, entire_feed[i]['title']])
+    table.add_row([i+1, entire_feed[i].author, entire_feed[i].title])
 
 print(table)
 
@@ -41,22 +41,34 @@ while True:
 
     if choice == 0:
         break
+    if choice > len(entire_feed):
+        choice = len(entire_feed)
 
     item_choice = entire_feed[choice - 1]
 
     print(
-        f"\nYou've chosen [{item_choice['title']}] by [{item_choice.author}].")
+        f"\nYou've chosen [{item_choice.title}] by [{item_choice.author}].")
 
     action = int(input("""Select an option.
 
     1) Pick another item.
     2) Open the link.
-    3) Exit the program
+    3) Add an RSS feed to the file!.  
+    4) Exit the program
     """))
 
     if action == 2:
-        webbrowser.open(item_choice['link'])
+        webbrowser.open(item_choice.link)
     elif action == 3:
+        user_link = input("Please enter the link here!")
+        try:
+            new_feed = Feed(user_link)
+            with open("links.txt", "w") as f:
+                f.write(user_link)
+            entire_feed += new_feed.updated_feed
+        except InvalidRSSError as i:
+            print("The RSS link either isn't up to standard or you didn't provide an RSS link. Please select another item.")
+    elif action == 4:
         break
     else:
         print("That's not a valid choice! Please pick again.")
